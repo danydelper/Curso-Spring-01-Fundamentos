02f659e85574c26f3b3a3fe38c778f47a7c7fce5
Delper <daniel@delper.com.br>
2021-12-28 04:00:27 -0300

    Versão 1.1 até Modulo 1.8

---
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -0,0 +1,67 @@
+package br.com.treinaweb.twclientes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+/*
+-->funciona os dois modos por ModelMap ou ModelView
+    @GetMapping("/mensagem")
+    public String mensagem(ModelMap modelMap) {
+        modelMap.addAttribute("mensagem", "Mensagem vinda do servidor");
+
+        return "mensagem";
+    }
+*/
+    @GetMapping("/mensagem")
+    public ModelAndView mensagem() {
+        ModelAndView modelAndView = new ModelAndView();
+        
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Outra mensagem vinda do servidor");
+    
+        return modelAndView;
+    }
+
+/*
+-->funciona os dois modos por ModelMap ou ModelView
+    @GetMapping("/saudacao/{nome}")
+    public ModelAndView saudacao(@PathVariable String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
+        
+        //Como passa no browser o nome 
+        //->http://localhost:8080/saudacao/Daniel
+
+    }
+*/
+
+@GetMapping("/saudacao")
+public ModelAndView saudacao(@RequestParam(required = false, defaultValue = "Treinaweb") String nome) {
+    ModelAndView modelAndView = new ModelAndView();
+
+    modelAndView.setViewName("saudacao");
+    modelAndView.addObject("nome", nome);
+
+    return modelAndView;
+    //Como passa no browser o nome 
+    //->http://localhost:8080/saudacao?nome=Daniel
+}
+
+}
