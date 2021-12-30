7124d2d332dae32ecae1d17af85a49d52ef14742
Delper <daniel@delper.com.br>
2021-12-30 01:24:18 -0300

    Versão 1.6 até módulo 1.13 - Cliando Cliente

--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -46,6 +47,24 @@ public class ClienteController {
 
     return modelAndView;
   }
+
+  @GetMapping("/cadastrar")
+  public ModelAndView cadastrar(){
+    ModelAndView modelAndView = new ModelAndView("cliente/cadastro");
+
+    modelAndView.addObject("cliente", new Cliente());
+    return modelAndView;
+  }
+
+  @PostMapping("/cadastrar")
+  public ModelAndView cadastrar(Cliente cliente) {
+    ModelAndView modelAndView = new ModelAndView("redirect:/cliente");
+
+    clienteRepository.save(cliente);
+    return modelAndView;
+  }
+
+
   
   
 }
